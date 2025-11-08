5d79c04dad8d487f8316eab7fe28b5642f684912
nougzarm <nougzarm@icloud.com>
2025-11-08 15:36:13 +0100

    Implémentation des fonctions ByteDecode et ByteEncode

--- conversion.py
+++ conversion.py
@@ -1,3 +1,5 @@
+CONST_d = 12
+
 def BitToBytes(b) -> bytes:
     if len(b) % 8 != 0:
         raise ValueError(f"Le tableau de bits n'a pas une longueur multiple de 8")
@@ -6,7 +8,6 @@ def BitToBytes(b) -> bytes:
     B = [0] * ( l//8 )
     for i in range(l):
         B[i//8] += b[i]*(2**(i%8))
-
     return bytes(B)
 
 def BytesToBits(B: bytes):
@@ -17,6 +18,27 @@ def BytesToBits(B: bytes):
             b[8*i + j] = C[i] % 2
             C[i] = C[i] // 2
     return b
+
+def ByteEncode(F, d=CONST_d) -> bytes:
+    b = [0] * (256*d)
+    for i in range(256):
+        a = F[i]
+        for j in range(d):
+            b[i*d + j] = a % 2
+            a = (a - b[i*d +j]) // 2
+    B = BitToBytes(b)
+    return B
+
+def ByteDecode(B: bytes, d=CONST_d, m=3329):
+    if len(B) // 32 != d:
+        raise ValueError(f"Mauvaise longueur")
+
+    F = [0] * 256
+    b = BytesToBits(B)
+    for i in range(256):
+        for j in range(d):
+            F[i] = (F[i] + b[i*d + j] * (2**j)) % m
+    return F
 
 if __name__ == '__main__':
     B = b"salut tous le monde. Comment allez vous"
