fb95b923f97761f9d1be1543ee36d648fb761f21
nougzarm <nougzarm@icloud.com>
2025-11-10 15:50:21 +0100

    Implémentation de PKE_KeyGen

---
+++ pke_scheme.py
@@ -0,0 +1,48 @@
+from xof import G, PRF
+from polynomial import SampleNTT, SamplePolyCBD, NTT, PolynomialNTT
+from conversion import ByteEncode
+
+""" 
+Correspond à l'algorithme 13
+"""
+def PKE_KeyGen(d, k, eta_1):
+    rh_gam = G(d + bytes([k]))
+    rho = rh_gam[:32]
+    gamma = rh_gam[32:]
+    N = 0
+
+    A_ntt = []
+    for i in range(k):
+        temp_line = []
+        for j in range(k):
+            temp_line.append(SampleNTT(rho + bytes([j]) + bytes([i])))
+        A_ntt.append(temp_line)
+
+    s = []
+    for i in range(k):
+        s.append(SamplePolyCBD(PRF(eta_1, gamma, bytes([N])), eta_1))
+        N += 1
+    
+    e = []
+    for i in range(k):
+        e.append(SamplePolyCBD(PRF(eta_1, gamma, bytes([N])), eta_1))
+        N += 1
+    
+    s_ntt = [NTT(s[i]) for i in range(k)]
+    e_ntt = [NTT(e[i]) for i in range(k)]
+
+    t_ntt = []
+    for i in range(k):
+        pol_temp = PolynomialNTT()
+        for j in range(k):
+            pol_temp = pol_temp + A_ntt[i][j] * s_ntt[j]
+        pol_temp = pol_temp + e_ntt[i]
+    
+    ek = b""
+    dk = b""
+    for i in range(k):
+        ek = ek + ByteEncode(t_ntt[i].coeffs)
+        dk = dk + ByteEncode(s_ntt[i].coeffs)
+    ek = ek + rho
+
+    return ek, dk
