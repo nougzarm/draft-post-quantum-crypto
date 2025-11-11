6964c874f9c33d02baf397bc84a609a9ea57cbaa
nougzarm <nougzarm@icloud.com>
2025-11-11 16:01:00 +0100

    Implémentation des algorithmes principaux du schéma ML-KEM

--- kem_scheme.py
+++ kem_scheme.py
@@ -1,3 +1,4 @@
+import secrets
 from pke_scheme import *
 from xof import H, G, J
 
@@ -56,9 +57,51 @@ def KEM_Decaps_internal(dk: bytes, c: bytes, k: int, eta_1: int, eta_2: int, d_u
     
     return K_prime
 
+""" 
+Algorithm 19 : ML-KEM.KeyGen()
+Generates an encapsulation key and a corresponding decapsulation key.
+
+Output : encapsulation key ek in B^(384*k + 32)
+output : decapsulation key dk in B^(768*k + 96)
+"""
+def KEM_KeyGen():
+    seed_d = secrets.token_bytes(32)
+    seed_z = secrets.token_bytes(32)
+
+    ek, dk = KEM_KeyGen_internal(seed_d, seed_z, k, eta_1)
+
+    return ek, dk
+
+""" 
+Algorithm 20 : ML-KEM.Encaps(ek)
+Uses the encapsulation key to generate a shared secret key and an associated ciphertext
+
+Input : encapsulation key ek in B^(384*k + 32)
+Output : shared secret key K in B^32
+Output : ciphertext c in B^(32 * (d_u*k + d_v))
+"""
+def KEM_Encaps(ek: bytes, k: int, eta_1: int, eta_2: int, d_u: int, d_v: int):
+    m = secrets.token_bytes(32)
+    K, c = KEM_Encaps_internal(ek, m, k, eta_1, eta_2, d_u, d_v)
+    return K, c
+
+""" 
+Algorithm 21 : ML-KEM.Decaps(dk, c)
+Uses the decapsulation key to produce a shared secret key from a ciphertext.
+
+Input : decapsulation key dk in B^(768*k + 96)
+Input : ciphertext c in B^(32 * (d_u*k + d_v))
+Output : shared secret key K in B^32
+"""
+def KEM_Decaps(dk: bytes, c: bytes, k: int, eta_1: int, eta_2: int, d_u: int, d_v: int):
+    K_prime = KEM_Decaps_internal(dk, c, k, eta_1, eta_2, d_u, d_v)
+    return K_prime
+
 # --- Exemple d'utilisation et tests ---
 if __name__ == '__main__':
     k, eta_1, eta_2, d_u, d_v = 3, 2, 2, 10, 4
+
+    # --- Test des algorithmes dits 'internal'
     seed = b"Salut de la part de moi meme lee"
 
     d = H(b"randomness d")
@@ -70,4 +113,11 @@ if __name__ == '__main__':
     K, c = KEM_Encaps_internal(ek, seed, k, eta_1, eta_2, d_u, d_v)
 
     K_decaps = KEM_Decaps_internal(dk, c, k, eta_1, eta_2, d_u, d_v)
+    assert K_decaps == K
+
+    # --- Test des algorithmes principaux
+    ek, dk = KEM_KeyGen()
+
+    K, c = KEM_Encaps(ek, k, eta_1, eta_2, d_u, d_v)
+    K_decaps = KEM_Decaps(dk, c, k, eta_1, eta_2, d_u, d_v)
     assert K_decaps == K
